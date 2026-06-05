3549a0747af3be1ca0c563083bb0fc2efd5d2005
ThaynaAlvarenga <thaynanunes1801@gmail.com>
2026-06-05 11:33:18 -0300

    fix: remove static export for vercel deployment

--- ntoria/next.config.ts
+++ ntoria/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  distDir: "dist",
   images: {
     unoptimized: true,
   },
